6a97cabd4d37fde9e5f03c55e1360fc77647a1ce
jonolds <jonolds@gmail.com>
2019-03-11 12:30:31 -0500

    win

--- src/main/java/SolsDriver.java
+++ src/main/java/SolsDriver.java
@@ -6,9 +6,9 @@ import sol4.Sol4;
 public class SolsDriver {
 
 	public static void main(String[] args) throws Exception {
-		Sol1.main("inout1/input", "inout1/output");
-		Sol2.main("inout2/input", "inout2/output");
-		Sol3.main("inout3/input", "inout3/output");
-		Sol4.main("inout4/input", "inout4/output");
+		Sol1.run("sol1/input", "sol1/output");
+		Sol2.run("sol2/input", "sol2/output");
+		Sol3.run("sol3/input", "sol3/output");
+		Sol4.run("sol4/input", "sol4/output");
 	}
 }
